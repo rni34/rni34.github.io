cd00c5ab08b09c7e719b3dab65ed5796d70d3a8b
Ryo <hakunishikawa@gmail.com>
2025-11-11 20:25:43 +1300

    Add test functions for Amazon Q review

---
+++ test-function.js
@@ -0,0 +1,11 @@
+// Test file to verify Amazon Q rule application
+
+function calculateSum(a, b) {
+    return a + b;
+}
+
+function multiplyNumbers(x, y) {
+    return x * y;
+}
+
+module.exports = { calculateSum, multiplyNumbers };
